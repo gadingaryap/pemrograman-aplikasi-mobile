e1c5818a13144595fc602e392e5f111fea555c92
gadingaryap <43136462+gadingaryap@users.noreply.github.com>
2018-09-10 17:53:18 +0700

    Add files via upload

---
+++ gading/src/gading/Gading.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gading;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Gading {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+     int jumlah = 5;
+            System.out.print("jumlah=");
+            System.out.print(jumlah);
+        for(int a=0;a<=jumlah;a++){
+           for(int b=0;b<a;b++){
+               System.out.print("*");
+           } 
+           System.out.println("");
+}
+    }
+}
